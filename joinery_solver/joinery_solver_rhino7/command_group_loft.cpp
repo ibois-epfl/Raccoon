49ad0e9aa79ce393f18028928372b965647aaf53
Petras Vestartas <petrasvestartas@gmail.com>
2021-11-25 23:23:59 +0100

    CleanUp

--- joinery_solver/joinery_solver_rhino7/command_group_loft.cpp
+++ joinery_solver/joinery_solver_rhino7/command_group_loft.cpp
@@ -33,77 +33,11 @@ bool UI(const CRhinoCommandContext& context, std::vector<ON_Curve*>& input_polyl
 		const  ON_Curve* crv = o.Curve();
 		if (!crv) continue;
 		if (!crv->IsClosed())continue;
-		//ON_SimpleArray<ON_3dPoint> points;
-		//crv->IsPolyline(&points);
-		//if (points.Count() < 3) continue;
-
-		////Construct CGAL_Polyline
-		//CGAL_Polyline pline;
-		//pline.reserve(points.Count());
-		//for (size_t i = 0; i < points.Count(); i++)
-		//	pline.emplace_back(points[i].x, points[i].y, points[i].z);
 		ON_Curve* curve = crv->DuplicateCurve();
 		input_polyline_pairs.emplace_back(curve);
 	}
 
 	if (input_polyline_pairs.size() == 0 || input_polyline_pairs.size() % 2 == 1) return false;
-
-
-	///////////////////////////////////////////////////////////////////////
-	////search_type | division_distance | shift
-	///////////////////////////////////////////////////////////////////////
-	//search_type = 2;
-	//division_distance = 1000;
-	//shift = 0.5;
-
-	///////////////////////////////////////////////////////////////////////
-	////Command Line Menu
-	///////////////////////////////////////////////////////////////////////
-	//CRhinoGetOption menu;
-	//menu.SetCommandPrompt(L"Parameter for the Joinery Solver");
-	//menu.AcceptNothing();
-
-	///////////////////////////////////////////////////////////////////////
-	////Iterate parameters until user stops
-	///////////////////////////////////////////////////////////////////////
-	//for (;;) {
-
-	//	menu.ClearCommandOptions();
-
-	//	//int nval_option_index = menu.AddCommandOptionInteger(RHCMDOPTNAME(L"Integer"), &search_type, L"0 - face_to_face, 1 - plane_to_face, 2 - all", 1, 99);
-
-
-	//	int search_type_value_index = menu.AddCommandOptionInteger(RHCMDOPTNAME(L"search_type"), &search_type, L"0 - face_to_face, 1 - plane_to_face, 2 - all", 0, 2);
-	//	int division_distance_value_index = menu.AddCommandOptionNumber(RHCMDOPTNAME(L"division_distance"), &division_distance, L"connection length divided by division_distance", FALSE, 0.001, 100000.0);
-	//	int shift_value_index = menu.AddCommandOptionNumber(RHCMDOPTNAME(L"shift"), &shift, L"joint shift value applied for all joints", FALSE, 0.0, 1.0);
-
-	//	CRhinoGet::result res = menu.GetOption();
-
-	//	if (res == CRhinoGet::nothing)
-	//		break;
-	//	if (res == CRhinoGet::cancel)
-	//		return false;
-	//	if (res != CRhinoGet::option)
-	//		return false;
-
-	//	const CRhinoCommandOption* option = menu.Option();
-	//	if (nullptr == option)
-	//		return false;
-
-	//	int option_index = option->m_option_index;
-
-	//	if (option_index == search_type_value_index)
-	//		continue; // nothing to do
-
-	//	if (option_index == division_distance_value_index)
-	//		continue; // nothing to do
-
-	//	if (option_index == shift_value_index)
-	//		continue; // nothing to do
-
-
-	//}
-
 
 
 	/////////////////////////////////////////////////////////////////////
@@ -114,12 +48,6 @@ bool UI(const CRhinoCommandContext& context, std::vector<ON_Curve*>& input_polyl
 
 
 }
-
-
-
-
-
-
 
 
 
@@ -301,7 +229,6 @@ CRhinoCommand::result command_group_loft::RunCommand(const CRhinoCommandContext&
 	/////////////////////////////////////////////////////////////////////
 
 	RhinoApp().Print(L"Input %d \n", input_polyline_pairs.size());
-	//RhinoApp().Print(L"Output %d \n", output_polyline_groups.size());
 
 	//Create layer
 	const wchar_t* layer_name = L"joinery_solver";
@@ -313,24 +240,6 @@ CRhinoCommand::result command_group_loft::RunCommand(const CRhinoCommandContext&
 	context.m_doc.GetDefaultObjectAttributes(attributes);
 	attributes.m_layer_index = layer_index;
 
-	//for (auto& output : output_polyline_groups) {
-
-	//	ON_SimpleArray<const CRhinoObject*> group_members(output.size());
-	//	for (int i = 0; i < output.size(); i++) {
-
-	//		ON_3dPointArray points;
-	//		points.Reserve(output[i].size());
-	//		for (size_t j = 0; j < output[i].size(); j++)
-	//			points.Append(ON_3dPoint(output[i][j].hx(), output[i][j].hy(), output[i][j].hz()));
-
-	//		ON_Polyline pline(points);
-	//		CRhinoCurveObject* curve_object = context.m_doc.AddCurveObject(pline, &attributes);
-	//		group_members.Append(curve_object);
-	//	}
-
-	//	int group_index = context.m_doc.m_group_table.AddGroup(ON_Group(), group_members);
-	//	if (group_index < 0) CRhinoCommand::failure;
-	//}
 	context.m_doc.Redraw();
 
 
